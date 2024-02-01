89e13ca5c398b08f22fb2d73ce74da07596c6fe5
Abdellah Kamil <abdellah.kamil2017@gmail.com>
2024-02-01 23:11:29 +0300

    Create palindrome_number.py

---
+++ palindrome_number/palindrome_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        orignal = x
+        reverse = 0;
+        while x > 0:
+            reverse = reverse * 10 + x % 10 
+            x //= 10
+        return orignal == reverse
