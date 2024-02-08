8b859af42beb58f60e8ad7e90783a4afd08d47bb
Abdellah Kamil <abdellah.kamil2017@gmail.com>
2024-02-08 21:33:58 +0300

    Create contain_duplicates.py

---
+++ Cotaine_Duplicates/contain_duplicates.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        counter = Counter(nums)
+        for value in counter.values():
+            if value >= 2:
+                return True
+        
+        return False
